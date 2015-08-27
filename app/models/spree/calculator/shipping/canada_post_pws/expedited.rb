2fd112816d6bb4508bc84c46b5962512392fa644
Frédéric boivin <fboivin63@gmail.com>
2015-08-27 11:29:42 -0400

    test shipping

--- app/models/spree/calculator/shipping/canada_post_pws/expedited.rb
+++ app/models/spree/calculator/shipping/canada_post_pws/expedited.rb
@@ -1,7 +1,7 @@
 module Spree
   module Calculator::Shipping
     module CanadaPostPws
-      class ExpeditedParcel < Spree::Calculator::Shipping::CanadaPostPws::Base
+      class Expedited < Spree::Calculator::Shipping::CanadaPostPws::Base
         def self.description
           I18n.t("canada_post_pws.expedited")
         end
--- app/models/spree/calculator/shipping/canada_post_pws/regular.rb
+++ app/models/spree/calculator/shipping/canada_post_pws/regular.rb
@@ -1,7 +1,7 @@
 module Spree
   module Calculator::Shipping
     module CanadaPostPws
-      class RegularParcel < Spree::Calculator::Shipping::CanadaPostPws::Base
+      class Regular < Spree::Calculator::Shipping::CanadaPostPws::Base
         def self.description
           I18n.t("canada_post_pws.regular")
         end
